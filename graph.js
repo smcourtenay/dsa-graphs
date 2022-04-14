3bed401b3374f182b058ace7b4d4778d656a47f3
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-04-14 12:08:00 -0700

    working on DFS for the graph

--- graph.js
+++ graph.js
@@ -16,26 +16,53 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+   }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray){
+      this.nodes.add(vertex);
+    }
+   }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+   }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) { 
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) { 
+    for (let neighbor in vertex.adjacent){
+      neighbor.adjacent.removeEdge(neighbor, vertex);
+    }
+    this.nodes.delete(vertex);
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start, visitedSet = new Set()) { 
+    if(start.adjacent.size === 0) return [start.value];
+
+    for (let neighbor in start.adjacent){
+      if(!visitedSet.has(neighbor)){
+        visitedSet.add(neighbor);
+        console.log("NEIGHBOR VALUE",neighbor.value)
+        return [neighbor.value, ...this.depthFirstSearch(neighbor, visitedSet)];
+      }
+    }
+  }
 
   /** traverse graph with BDS and returns array of Node values */
   breadthFirstSearch(start) { }
